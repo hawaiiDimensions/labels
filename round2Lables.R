d9666d6f201fd2d4a39c7a637bd2fcad6bd591a5
ajrominger <ajrominger@gmail.com>
2016-04-19 17:04:17 -0700

    now makes all round 2 labels

--- round2Lables.R
+++ round2Lables.R
@@ -1,1 +1,19 @@
 ## example of how to use `makeLabels' and also making labels for round 2
+
+source('~/Dropbox/hawaiidimensions/labels/makeLabels.R')
+
+## old call...reminder of general functionality
+# makeLabels(c(5473, 5330, 5158, 5506, 5481, 10039, 6299, 6310, 5509), 
+#            '~/Dropbox/hawaiiDimensions/labels', 'labels_2016-02-09', 
+#            repID=c(26, 18, 12, 10, 9, 9, 18, 16, 1))
+
+
+## get copy of database and find round 2
+db <- RCurl::getURL('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
+db <- read.csv(textConnection(db), as.is=TRUE)
+hdimRound2 <- db$HDIM[as.Date(db$Date, format='%m/%d/%Y') > as.Date('7/1/15', format='%m/%d/%Y')]
+hdimRound2 <- hdimRound2[!is.na(hdimRound2)]
+
+
+## make labels for those hdim, 18 each
+makeLabels(hdimRound2, '~/Dropbox/hawaiiDimensions/labels', 'labels_round2', repID=18)
