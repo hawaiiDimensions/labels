711089caa9bb7a5cbd75cd8aea5d1d19d6ebc9c7
ajrominger <ajrominger@gmail.com>
2016-06-09 14:10:53 -0700

    making labels for first round of special project

--- round2Lables.R
+++ round2Lables.R
@@ -8,12 +8,19 @@ source('~/Dropbox/hawaiidimensions/labels/makeLabels.R')
 #            repID=c(26, 18, 12, 10, 9, 9, 18, 16, 1))
 
 
-## get copy of database and find round 2
-db <- RCurl::getURL('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
-db <- read.csv(textConnection(db), as.is=TRUE)
-hdimRound2 <- db$HDIM[as.Date(db$Date, format='%m/%d/%Y') > as.Date('7/1/15', format='%m/%d/%Y')]
-hdimRound2 <- hdimRound2[!is.na(hdimRound2)]
+# # get copy of database and find round 2
+# db <- RCurl::getURL('https://docs.google.com/spreadsheets/d/1Ve2NZwNuGMteQDOoewitaANfTDXLy8StoHOPv7uGmTM/pub?output=csv')
+# db <- read.csv(textConnection(db), as.is=TRUE)
+# hdimRound2 <- db$HDIM[as.Date(db$Date, format='%m/%d/%Y') > as.Date('7/1/15', format='%m/%d/%Y')]
+# hdimRound2 <- hdimRound2[!is.na(hdimRound2)]
 
 
-## make labels for those hdim, 18 each
-makeLabels(hdimRound2, '~/Dropbox/hawaiiDimensions/labels', 'labels_round2', repID=18)
+# ## make labels for those hdim, 18 each
+# makeLabels(hdimRound2, '~/Dropbox/hawaiiDimensions/labels', 'labels_round2', repID=18)
+
+## make some specific labels
+these.hdim <- c(8007, 8005, 8006, 6226, 8000, 8001, 8002, 8003, 8004, 8008, 8009)
+these.num <- c(16, 11, 15, rep(18, 8))
+makeLabels(these.hdim, 
+           '~/Dropbox/hawaiiDimensions/labels', 'labels_2016-06-10', 
+           repID=these.num)
